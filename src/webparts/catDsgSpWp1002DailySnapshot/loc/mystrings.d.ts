88a7bfc6b9404be67a2b738103b9ab6a3d34a63a
Roger Zhang <zhangqinyuan2013@live.cn>
2018-05-31 13:23:55 +0800

    Initialize Snapshot SP WP

---
+++ src/webparts/catDsgSpWp1002DailySnapshot/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICatDsgSpWp1002DailySnapshotWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CatDsgSpWp1002DailySnapshotWebPartStrings' {
+  const strings: ICatDsgSpWp1002DailySnapshotWebPartStrings;
+  export = strings;
+}
