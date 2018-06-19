28aa2c372f5b0d8320255ec219e9f811432d71d7
Roger Zhang <zhangqinyuan2013@live.cn>
2018-06-19 15:01:25 +0800

    Add Chinese Support；Update Configurations

--- src/webparts/catDsgSpWp1002DailySnapshot/loc/en-us.js
+++ src/webparts/catDsgSpWp1002DailySnapshot/loc/en-us.js
@@ -1,8 +1,8 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
+    "PropertyPaneDescription": "Settings",
+    "BasicGroupName": "Basic Settings",
+    "DescriptionFieldLabel": "Introduction",
     "catDsgSpWp1002DailySnapshotDropdownLabelListName":"List Name",
     "catDsgSpWp1002DailySnapshotListNameRequiredMessage":"List Name is required",
     "catDsgSpWp1002DailySnapshotAddPhotoLinkText":"Add Photo",
--- src/webparts/catDsgSpWp1002DailySnapshot/loc/zh-cn.js
+++ src/webparts/catDsgSpWp1002DailySnapshot/loc/zh-cn.js
@@ -1,14 +1,14 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
-    "catDsgSpWp1002DailySnapshotDropdownLabelListName":"List Name",
-    "catDsgSpWp1002DailySnapshotListNameRequiredMessage":"List Name is required",
-    "catDsgSpWp1002DailySnapshotAddPhotoLinkText":"Add Photo",
-    "catDsgSpWp1002DailySnapshotFieldLabelFileTypes":"File Types (Separate each with semicolon)",
-    "catDsgSpWp1002DailySnapshotFileTypesRequiredMessage":"File Types Field is required",
-    "catDsgSpWp1002DailySnapshotListNotFoundMessage":"The List was Not Found",
-    "catDsgSpWp1002DailySnapshotBadRequestMessagePrefix":"Bad Request! \r\n"
+    "PropertyPaneDescription": "设置",
+    "BasicGroupName": "基本设置",
+    "DescriptionFieldLabel": "简介",
+    "catDsgSpWp1002DailySnapshotDropdownLabelListName":"列表名称",
+    "catDsgSpWp1002DailySnapshotListNameRequiredMessage":"列表名称是必填项",
+    "catDsgSpWp1002DailySnapshotAddPhotoLinkText":"添加图片",
+    "catDsgSpWp1002DailySnapshotFieldLabelFileTypes":"文件类型 (请用分号分隔每个文件类型)",
+    "catDsgSpWp1002DailySnapshotFileTypesRequiredMessage":"文件类型是必填项",
+    "catDsgSpWp1002DailySnapshotListNotFoundMessage":"列表项没有找到",
+    "catDsgSpWp1002DailySnapshotBadRequestMessagePrefix":"请求错误! \r\n"
   }
 });
